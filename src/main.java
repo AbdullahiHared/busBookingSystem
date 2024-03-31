dd359972f1cffc06284a4a5efe4daffec076364e
Abdullahi <muxsinxareed6@gmail.com>
2024-03-31 12:58:37 +0200

    prompt role choices

--- src/main.java
+++ src/main.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class main {
+    static Scanner mainScanner = new Scanner(System.in); // Declaring Scanner globally
+
+    static String[][] seats = {
+            {"0", "0", "0", "0"},
+            {"0", "0", "0", "0"},
+            {"0", "0", "0", "0"},
+            {"0", "0", "0", "0"},
+            {"0", "0", "0", "0"}
+    };
+
+    static int promptUserForRoleChoice() {
+        System.out.print("Choose from below:\t");
+        System.out.println("0.Exit: \t 1. Bus Inspector: \t 2. Passenger: ");
+        System.out.print(": ");
+        int choice = mainScanner.nextInt();
+        return choice;
+    }
+
+    public static void main(String[] args) {
+        promptUserForRoleChoice();
+    }
 }
