a26453886966c99376c853d9385f22eca23c504c
Abdullahi <muxsinxareed6@gmail.com>
2024-05-05 20:12:46 +0200

    Added JavaDoc comments to all methods

--- src/mainClass.java
+++ src/mainClass.java
@@ -2,6 +2,12 @@ import java.time.LocalDate;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
+
+/**
+ * This is a simple bus booking system that allows passengers to book seats on a bus.
+ * The system also allows the bus inspector to view the current profit, sort customers by age,
+ * and view the current passengers on board.
+ */
 public class mainClass {
     static Scanner mainScanner = new Scanner(System.in); // Declaring Scanner globally
     static int profit = 0;
@@ -17,6 +23,10 @@ public class mainClass {
         startBusService();
     }
 
+    /**
+     * This method starts the bus service and prompts the user to choose a role.
+     * The user can choose to be a bus inspector or a passenger.
+     */
     static void startBusService() {
         int choice = promptUserForRoleChoice(); // Get the user's choice
         switch (choice) {
@@ -31,6 +41,11 @@ public class mainClass {
         }
     }
 
+    /**
+     * This method prompts the user to choose a role.
+     *
+     * @return the user's choice
+     */
     static int promptUserForRoleChoice() {
         int choice;
         while (true) {
@@ -44,6 +59,9 @@ public class mainClass {
         }
     }
 
+    /**
+     * This method prints the choices for the user to choose a role.
+     */
     static void printRolesChoices() {
         System.out.println("Choose from below:");
         System.out.println("1. Bus  Inspector:");
@@ -52,22 +70,38 @@ public class mainClass {
         System.out.print(": ");
     }
 
+    /**
+     * This method prints the front section of the bus.
+     */
+
     static void printBusFrontSection() {
         System.out.println("   /\\    /\\");
         System.out.println("__>-<____>-<__");
     }
+
+    /**
+     * This method prints the back section of the bus.
+     */
 
     public static void printBusBack() {
         System.out.println("\n|----------------|");
         System.out.println("\\-/_\\--------/_\\-/\n");
     }
 
+    /**
+     * This method prints the wheels of the bus.
+     *
+     * @param row the row number
+     */
     static void printWheels(int row) {
         if (row == 1 || row == 3) {
             System.out.print("\n()--------------()");
         }
     }
 
+    /**
+     * This method prints the bus seats.
+     */
     static void printBusSeats() {
         printBusFrontSection(); // Print the front section of the bus
         for (int i = 0; i < busSeats.length; i++) {
@@ -88,6 +122,9 @@ public class mainClass {
     }
 
 
+    /**
+     * This method starts the customer service and prompts the user to choose an action.
+     */
     static void startCustomerService() {
         switch (getCustomerChoice()) {
             case 1:
@@ -110,6 +147,11 @@ public class mainClass {
         }
     }
 
+    /**
+     * This method gets the customer's choice.
+     *
+     * @return the customer's choice
+     */
     static int getCustomerChoice() {
         int customerChoice;
         System.out.println("0.Exit");
@@ -129,6 +171,11 @@ public class mainClass {
         }
     }
 
+    /**
+     * This method prompts the user to choose a seat.
+     *
+     * @return the seat number
+     */
     static boolean checkSeatBooked(int seat) {
         for (String[] busRow : busSeats) {
             for (String s : busRow) {
@@ -140,7 +187,20 @@ public class mainClass {
         return true;
     }
 
-
+    /**
+     * Books a seat for a customer, collects user information, reserves the seat,
+     * calculates ticket price based on customer age, saves passenger data,
+     * displays booking information, and starts customer service.
+     * <p>
+     * This method performs the following steps:
+     * 1. Collects user information using {@link #getUserInfo()}.
+     * 2. Prompts the user to choose a seat using {@link #getCustomerSeatChoice()}.
+     * 3. Reserves the selected seat using {@link #reserveSeat(int)}.
+     * 4. Calculates the ticket price based on the customer's age.
+     * 5. Saves the passenger's data using {@link #addCustomerData(String, String, String)}.
+     * 6. Displays booking information using {@link #displayBookingInfo(String, String, int, double)}.
+     * 7. Starts the customer service again by calling {@link #startCustomerService()}.
+     */
     static void bookSeat() {
         String[] userInfo = getUserInfo();
         // get the seat number from the user
@@ -173,6 +233,15 @@ public class mainClass {
         startCustomerService();
     }
 
+
+    /**
+     * This method displays the booking information.
+     *
+     * @param fullName   the full name of the passenger
+     * @param birthDate  the birthdate of the passenger
+     * @param seatNumber the seat number
+     * @param price      the ticket price
+     */
     static void displayBookingInfo(String fullName, String birthDate, int seatNumber, double price) {
         System.out.println("Booking Info: ");
         System.out.println("Full Name: " + fullName);
@@ -182,13 +251,22 @@ public class mainClass {
         System.out.println("Welcome on board <>");
     }
 
-
+    /**
+     * This method gets the user's information.
+     *
+     * @return the user's information ie full name and birthdate
+     */
     static String[] getUserInfo() {
         String fullName = userName();
         int birthDate = promptPassengerForBirthDate();
         return new String[]{fullName, Integer.toString(birthDate)};
     }
 
+    /**
+     * This method gets the user's name.
+     *
+     * @return the user's name ie first name and last name
+     */
     static String userName() {
         System.out.print("Enter your first name: ");
         String firstName = mainScanner.next(); //
@@ -204,12 +282,23 @@ public class mainClass {
         return firstName + " " + lastName;
     }
 
+    /**
+     * This method checks if the date format is valid.
+     *
+     * @param date the date to check
+     * @return true if the date format is valid, otherwise false
+     */
     static boolean isValidDateFormat(String date) {
         // Regular expression to match the format yyyyMMdd
         String regex = "\\d{8}"; // This matches exactly 8 digits
         return date.matches(regex);
     }
 
+    /**
+     * This method prompts the passenger for their birthdate.
+     *
+     * @return the passenger's birthdate
+     */
     static int promptPassengerForBirthDate() {
         System.out.println("Please enter your birth date in the format yyyyMMdd:");
         String userInput = mainScanner.next();
@@ -221,6 +310,11 @@ public class mainClass {
         }
     }
 
+    /**
+     * This method gets the customer's seat choice.
+     *
+     * @return the customer's seat choice
+     */
     static int getCustomerSeatChoice() {
         int windowSelectionAnswer = getUserWindowSelection();
         int seatChoice = 0;
@@ -239,6 +333,11 @@ public class mainClass {
         return seatChoice;
     }
 
+    /**
+     * This method gets the customer's seat choice.
+     *
+     * @return the customer's seat choice
+     */
     static int customerSeatChoice() {
         int seatChoice;
         while (true) {
@@ -260,7 +359,11 @@ public class mainClass {
         return seatChoice;
     }
 
-
+    /**
+     * This method reserves a seat for a customer.
+     *
+     * @param seat the seat number
+     */
     static void reserveSeat(int seat) {
         String seatChoice = Integer.toString(seat);
         boolean seatReserved = false; // Flag to track if seat is reserved
@@ -283,7 +386,9 @@ public class mainClass {
         }
     }
 
-
+    /**
+     * This method gets the unbooked seats.
+     */
     static void getUnbookedSeats() {
         StringBuilder availableSeats = new StringBuilder();
         for (String[] busSeat : busSeats) {
@@ -298,6 +403,9 @@ public class mainClass {
         System.out.println(availableSeats);
     }
 
+    /**
+     * This method gets the unbooked window seats.
+     */
     static void getUnbookedWindowSeats() {
         StringBuilder availableWindowSeats = new StringBuilder();
         for (int i = 0; i < 4; i++) {
@@ -314,6 +422,11 @@ public class mainClass {
         System.out.println(availableWindowSeats);
     }
 
+    /**
+     * This method gets the user's window selection.
+     *
+     * @return the user's window selection
+     */
     static int getUserWindowSelection() {
         int selection = 0; // Default value
         boolean validInput = false;
@@ -338,6 +451,12 @@ public class mainClass {
         return selection;
     }
 
+    /**
+     * This method checks if the customer is an adult.
+     *
+     * @param birthDate the birthdate of the customer
+     * @return true if the customer is an adult, otherwise false
+     */
     static boolean isCustomerAdult(int birthDate) {
         int birthYear = birthDate / 10000; // Extract the birth year from the full date
 
@@ -349,6 +468,12 @@ public class mainClass {
         return age >= 18;
     }
 
+    /**
+     * This method gets the customer's age.
+     *
+     * @param birthDate the birthdate of the customer
+     * @return the customer's age
+     */
     static int getCustomerAge(int birthDate) {
         int birthYear = birthDate / 10000; // Extract the birth year from the full date
 
@@ -357,6 +482,13 @@ public class mainClass {
         return currentYear - birthYear;
     }
 
+    /**
+     * This method adds the customer data to the customers array.
+     *
+     * @param seatNumber the seat number
+     * @param birthDate  the birthdate
+     * @param fullName   the full name
+     */
     static void addCustomerData(String seatNumber, String birthDate, String fullName) {
         boolean dataAdded = false;
         try {
@@ -375,7 +507,9 @@ public class mainClass {
         }
     }
 
-
+    /**
+     * This method finds the customer data based on the birth ate and seat number.
+     */
     static void findCustomerData() {
         int birthDate = promptPassengerForBirthDate();
         System.out.println("Please enter the seat you had booked: ");
@@ -393,6 +527,9 @@ public class mainClass {
         }
     }
 
+    /**
+     * This method unbooks a seat for a customer.
+     */
     static void unBookSeat() {
         int birthDate = promptPassengerForBirthDate();
         System.out.print("Please Enter Which seat number you had: ");
@@ -424,13 +561,23 @@ public class mainClass {
         }
     }
 
+    /**
+     * This method updates the seat reservation.
+     *
+     * @param seat the seat number
+     */
     static void updateSeatReservation(int seat) {
         for (String[] seatPosition : busSeats) {
             seatPosition[seat - 1] = String.valueOf(seat); // Corrected this line
         }
     }
 
-    // remove customer data
+    /**
+     * This method removes the customer data from the customers array.
+     *
+     * @param seatNumber the seat number
+     * @param birthDate  the birthdate
+     */
     static void removeCustomerData(int seatNumber, int birthDate) {
         for (String[] customer : customers) {
             if (customer[0] != null && customer[0].equals(String.valueOf(seatNumber)) && customer[1].equals(String.valueOf(birthDate))) {
@@ -441,11 +588,17 @@ public class mainClass {
         }
     }
 
-
+    /**
+     * This method allows the bus inspector to view the current profit, sort customers by age,
+     * and view the current passengers on board.
+     */
     static void busInspector() {
         getInspectorChoice();
     }
 
+    /**
+     * This method prints the choices for the bus inspector.
+     */
     static void printInspectorChoices() {
         System.out.println("0. Exit");
         System.out.println("1. Display current profit:");
@@ -455,6 +608,9 @@ public class mainClass {
         System.out.print("> ");
     }
 
+    /**
+     * This method gets the bus inspector's choice.
+     */
     static void getInspectorChoice() {
         printInspectorChoices();
         int inspectorChoice = mainScanner.nextInt();
@@ -480,6 +636,9 @@ public class mainClass {
         }
     }
 
+    /**
+     * This method prints the current passengers on board.
+     */
     static void getCurrentCustomers() {
         int currentCount = 0;
         for (String[] customer : customers) {
@@ -498,7 +657,9 @@ public class mainClass {
     }
 
 
-    //sort customers by age
+    /**
+     * This method sorts the customers by age.
+     */
     static void sortCustomersByAge() {
         System.out.println("Customers Sorted By Age: ");
         for (int i = 0; i < customers.length; i++) {
@@ -515,6 +676,7 @@ public class mainClass {
                 }
             }
         }
+
 
         // Print all customers after sorting
         for (String[] customer : customers) {
